93c0af665a6cc4fc52090862eaa043e3413924c3
Timm Murray <tmurray@wumpus-cave.net>
2022-01-22 09:20:54 -0600

    cmake setup with hello

---
+++ src/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* Function int main(int argc, char **argv) {{{ */
+int main(int argc, char **argv)
+{
+    printf( "Hello, world\n" );
+
+	return 0;
+} /* }}} */
+
+
+/* Modeline for ViM {{{
+ * vim:set ts=4:
+ * vim600:fdm=marker fdl=0 fdc=3:
+ * }}} */
+
+
